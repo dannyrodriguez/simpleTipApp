070a6aae275942a56881c60e8edc152fa0202c9b
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-05 00:19:30 -0500

    Initial Commit

---
+++ TipApp/JavaScript.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+var calculateTip = function () {
+    "use strict";
+
+    var bill = Number(document.getElementById("bill").value),
+        tip = Number(document.getElementById("tip").value),
+        percent = tip / 100,
+        calculatedTip = bill * percent,
+        total = bill + calculatedTip;
+
+    document.getElementById("result").innerHTML = "The tip is: $" + calculatedTip + " and your total is: $" + total;
+}
